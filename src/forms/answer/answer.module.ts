1a2181144769622ff3b750b9ab18676568e45302
Lorenzo Ribeiro <lorenzog.ribeiro2@gmail.com>
2025-09-16 18:45:18 -0300

    fix: reorder import statements in AnswerModule for consistency

--- src/forms/answer/answer.module.ts
+++ src/forms/answer/answer.module.ts
@@ -1,6 +1,6 @@
-import { AnswerService } from './answer.service';
 import { Module } from '@nestjs/common';
 import { AnswerController } from './answer.controller';
+import { AnswerService } from './answer.service';
 
 @Module({
   imports: [],
